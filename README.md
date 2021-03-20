f81d936ab3f84c70928bda5b9fa3704912ea3141
Ubuntu <ubuntu@ip-172-31-26-197.eu-central-1.compute.internal>
2021-03-20 16:08:44 +0000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Install Single Node Rancher
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Install Single Node Rancher"
+
+docker run -d --restart=unless-stopped \
+  -p 80:80 -p 443:443 \
+  -v /opt/rancher:/var/lib/rancher \
+  --privileged \
+  rancher/rancher:latest
+
+docker ps -a
+
